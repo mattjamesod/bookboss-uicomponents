f5fdf5f9ffdf4015bfe3723c78abc50bef84d46f
Matthew O'Donnell <matthew.james.od@gmail.com>
2022-01-03 15:52:24 +0000

    made popup public

--- Sources/BookBossUIComponents/Popup.swift
+++ Sources/BookBossUIComponents/Popup.swift
@@ -6,16 +6,16 @@ extension View {
     }
 }
 
-struct Popup<T: View>: ViewModifier {
+public struct Popup<T: View>: ViewModifier {
     let popup: T
     @Binding var isPresented: Bool
 
-    init(isPresented: Binding<Bool>, @ViewBuilder content: () -> T) {
+    public init(isPresented: Binding<Bool>, @ViewBuilder content: () -> T) {
         self._isPresented = isPresented
         popup = content()
     }
 
-    func body(content: Content) -> some View {
+    public func body(content: Content) -> some View {
         content
             .overlay {
                 popupContent()
